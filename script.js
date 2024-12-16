a4494ad23c6e46bc213ac91964d8aecdfc459ba6
JasonEllsworth <jasonells48@icloud.com>
2024-12-16 14:06:24 +0800

    add program expectations to README and added the basic skeleton for the calculator in the script.

---
+++ script.js
@@ -0,0 +1,40 @@
+function evaluate() {
+    let input = [];
+    let operator;
+
+    switch (operator) {
+        case '+':
+            divide();
+            break;
+        case '-':
+            subtract();
+            break;
+        case '*':
+            multiply();
+            break;
+        case '/':
+            addition();
+            break;
+    }
+
+    //Each function takes in two parameters and returns their result.
+    function divide() {
+        a / b;
+    }
+
+    function addition(){
+        a + b;
+
+    }
+
+    function subtract() {
+        a - b;
+
+    }
+
+    function multiply() {
+
+        a * b;
+
+    }
+}
